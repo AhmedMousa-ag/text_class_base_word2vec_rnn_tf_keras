e903ebcf51ed00a382a0ca132238c5114c5f88ee
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-12-01 19:19:36 +0200

    added comments

--- download_embedding.sh
+++ download_embedding.sh
@@ -1,7 +1,7 @@
 
 
-export embed_dim=100 #Must be declared. #available dims are 100, 300 and 500 
-export embed_file_name=enwiki_20180420_"$embed_dim"d.txt
+export embed_dim=100 #Must be declared as it will be used for training. #available dims are 100, 300 and 500 
+export embed_file_name=enwiki_20180420_"$embed_dim"d.txt #Must be decleared as it will be used during training
 
 # For refrencess please visit https://wikipedia2vec.github.io/wikipedia2vec/pretrained/
 
@@ -12,5 +12,5 @@ bzip2 -d -q /tmp/"$embed_file_name".bz2
 cp /tmp/"$embed_file_name" /opt/app/model/artifacts/trained_embed/
 
 
-echo 'export embed_dim=$embed_dim' >> ~/.profile
-echo 'export embed_file_name=$embed_file_name' >> ~/.profile
+echo 'export embed_dim=$embed_dim' >> ~/.profile #To keep env variable on the system after restarting
+echo 'export embed_file_name=$embed_file_name' >> ~/.profile #To keep env variable on the system after restarting
