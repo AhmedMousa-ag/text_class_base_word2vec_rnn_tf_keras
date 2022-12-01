61264f3c3b4f508672a495d6db7b789309bade7e
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-12-01 10:00:43 +0200

    added trained embedding word2vec 300 dim

--- app/Utils/model_builder.py
+++ app/Utils/model_builder.py
@@ -1,17 +1,20 @@
-import logging, os
+from Utils.preprocess.preprocess import prep_TEXT
+import config
+import numpy as np
+from tensorflow.keras.metrics import Recall, Precision
+from tensorflow.keras.layers import Dense, Bidirectional, GRU, Flatten, Embedding, Input
+import tensorflow as tf
+import os
+import logging
+import os
 logging.disable(logging.WARNING)
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
-import os
-import tensorflow as tf
-from tensorflow.keras.layers import Dense, Bidirectional, GRU, Flatten, Embedding, Input
-from tensorflow.keras.metrics import Recall, Precision
-import numpy as np
-import config
-from Utils.preprocess.preprocess import prep_TEXT
 
 MODEL_NAME = config.MODEL_NAME
 MODEL_SAVE_PATH = config.MODEL_SAVE_PATH
+TRAINED_EMBEDD_PATH = config.TRAINED_EMBEDD_PATH
+
 seed = config.RAND_SEED
 tf.random.set_seed(seed)
 
@@ -20,33 +23,38 @@ class RNN_pretrained_embed():
     def __init__(self):
         pass
 
-    def __build_model_compile(self, num_y_classes, num_layers, neurons_num, embed_lay_output,learning_rate):
+    def __build_model_compile(self, num_y_classes, num_layers, neurons_num, embed_lay_output, learning_rate):
         """This fucntion builds the model and compile it"""
         # tensorflow hub universal sentence encoder
         optimizer = tf.keras.optimizers.Adam(learning_rate)
         metrics = [Precision(),
                    Recall()
                    ]
-        
+
         text_vectorizer = prep_TEXT.load_text_vectorizer()
-        
-        max_vocab_length = len(text_vectorizer.get_vocabulary())
 
-        max_length = len(tf.squeeze(text_vectorizer(["dsads"]))) #We defined output lenght during preprocessing, now getting it for embedding layer
+        voc = text_vectorizer.get_vocabulary()
 
-        embed_layer = Embedding(input_dim=max_vocab_length, # set input shape
-                             output_dim=embed_lay_output, # set size of embedding vector
-                             input_length=max_length, # how long is each input
-                             name="Embedding_Layer") 
+        # We defined output lenght during preprocessing, now getting it for embedding layer
+        max_length = len(tf.squeeze(text_vectorizer(["dsads"])))
+
+        embedding_dim = 300
+        num_tokens = len(voc) + 2
+        embed_layer = Embedding(num_tokens,
+                                embedding_dim,
+                                embeddings_initializer=tf.keras.initializers.Constant(
+                                    self.get_trained_embedd_matrix(voc=voc, embed_dim=embedding_dim)),
+                                trainable=False,
+                                name="Embedding_Layer")
 
         model = tf.keras.Sequential()
-        model.add(Input(shape=[1,],dtype=tf.string))
+        model.add(Input(shape=(1,), dtype=tf.string))
         model.add(text_vectorizer)
         model.add(embed_layer)
 
         for i in range(num_layers):
-            model.add(Bidirectional(GRU(neurons_num,return_sequences=True),
-                        name=f"Bidirectional_layer_{i}"))
+            model.add(Bidirectional(GRU(neurons_num, return_sequences=True),
+                                    name=f"Bidirectional_layer_{i}"))
 
         model.add(Flatten())
 
@@ -54,13 +62,14 @@ class RNN_pretrained_embed():
             model.add(Dense(num_y_classes, activation='softmax'))
             loss = tf.keras.losses.CategoricalCrossentropy()
             model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
-        else:                             
+        else:
             model.add(Dense(1, activation='sigmoid'))
             loss = tf.keras.losses.BinaryCrossentropy()
             model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
         return model
 
-    def fit(self, x_train, y_train, x_val=None, y_val=None,call_backs=[],
+
+    def fit(self, x_train, y_train, x_val=None, y_val=None, call_backs=[],
             epochs=10,
             num_layers=2,
             neurons_num=50,
@@ -69,29 +78,58 @@ class RNN_pretrained_embed():
             ):
         num_classes = len(np.unique(y_train))
 
-
-
         self.model = self.__build_model_compile(
             num_y_classes=num_classes, num_layers=num_layers,
             neurons_num=neurons_num, embed_lay_output=embed_lay_output,
             learning_rate=learning_rate)
-            
-        if num_classes>2:
-            y_train = tf.squeeze(tf.one_hot(y_train,num_classes))
+
+        if num_classes > 2:
+            y_train = tf.squeeze(tf.one_hot(y_train, num_classes))
             if not y_val is None:
-                y_val = tf.squeeze(tf.one_hot(y_val,num_classes))
+                y_val = tf.squeeze(tf.one_hot(y_val, num_classes))
 
         if x_val is None:
-            self.model.fit(x_train, y_train, epochs=epochs,callbacks=call_backs)
+            self.model.fit(x_train, y_train, epochs=epochs,
+                           callbacks=call_backs)
         else:
-            
+
             self.model.fit(x_train, y_train, epochs=epochs, validation_data=(
-                x_val, y_val), validation_steps=len(x_val),callbacks=call_backs)
+                x_val, y_val), validation_steps=len(x_val), callbacks=call_backs)
 
         return self.model
 
-    def get_trained_embedd(self):
-        pass
+
+    def get_trained_embedd_matrix(self, voc, embed_dim, trained_embedd_path=None):
+        train_embed_path = trained_embedd_path if trained_embedd_path else TRAINED_EMBEDD_PATH
+        path = os.path.join(train_embed_path,"glove.6B.300d.txt")
+
+        embeddings_index = {}
+        with open(path) as f:
+            for line in f:
+                word, coefs = line.split(maxsplit=1)
+                coefs = np.fromstring(coefs, "f", sep=" ")
+                embeddings_index[word] = coefs
+        print("Found %s word vectors." % len(embeddings_index))
+
+        num_tokens = len(voc) + 2
+        embedding_dim = embed_dim
+        hits = 0
+        misses = 0
+        word_index = dict(zip(voc, range(len(voc))))
+
+        embedding_matrix = np.zeros((num_tokens, embedding_dim))
+        for word, i in word_index.items():
+            embedding_vector = embeddings_index.get(word)
+            if embedding_vector is not None:
+                # Words not found in embedding index will be all-zeros.
+                # This includes the representation for "padding" and "OOV"
+                embedding_matrix[i] = embedding_vector
+                hits += 1
+            else:
+                misses += 1
+
+        return embedding_matrix
+
 
     def save_model(self, save_path=MODEL_SAVE_PATH):
         path = os.path.join(save_path, MODEL_NAME)
