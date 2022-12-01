c0efe3a3d49fd40570b23319f26b39155e70d895
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-12-01 13:12:43 +0200

    modified download_embedding.sh

--- download_embedding.sh
+++ download_embedding.sh
@@ -1,9 +1,14 @@
 
 
-export embed_dim=300
+export embed_dim=100 #Must be declared. 
+export embed_file_name=enwiki_20180420_"$embed_dim"d.txt
 
-wget http://nlp.stanford.edu/data/glove.6B.zip /temp/
+wget http://wikipedia2vec.s3.amazonaws.com/models/en/2018-04-20/enwiki_20180420_"$embed_file_name".bz2 /temp/
 
-unzip -q /temp/glove.6B.zip
+unzip -q /temp/"$embed_file_name".bz2
 
-cp /temp/glove.6B."$embed_dim"d.txt /opt/app/model/artifacts/trained_embed/
+cp /temp/"$embed_file_name" /opt/app/model/artifacts/trained_embed/
+
+
+echo "export embed_dim=$embed_dim" > ~/.profile
+echo "export embed_file_name=$embed_file_name" > ~/.profile
